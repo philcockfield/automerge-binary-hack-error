0129acbacb48b8129aa618d1f62ae68c3b268ecb
Phil Cockfield <phil@cockfield.net>
2024-09-13 10:36:32 +1200

    sample in client

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,16 +1,24 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
+import { fromBinary, Repo, toBinary } from '../common';
 
-function App() {
-  const [count, setCount] = useState(0);
+export type TDoc = { count: number; text?: string };
+const repo = new Repo({ network: [] });
+
+export function App() {
+  /**
+   * Test: to/from binary within browser.
+   */
+  useEffect(() => {
+    const a = repo.create<TDoc>({ count: 123 });
+    const binary = toBinary<TDoc>(a);
+    const b = fromBinary(repo, binary);
+
+    console.log('b.docSync()', { ...b.docSync() });
+  }, []);
 
   return (
     <>
       <h1>Debug Sample</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-      </div>
     </>
   );
 }
-
-export default App;
