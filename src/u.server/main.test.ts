dabc1ff70c5a2647c93671968af490de89b024d8
Phil Cockfield <phil@cockfield.net>
2024-09-13 09:54:12 +1200

    Update main.test.ts

--- src/u.server/main.test.ts
+++ src/u.server/main.test.ts
@@ -4,9 +4,11 @@ import { Repo } from '../common';
 export type TDoc = { count: number; text?: string };
 
 describe('Automerge basics', () => {
+  const repo = new Repo({ network: [] });
+
   it('simple document', () => {
-    const repo = new Repo({ network: [] });
-    const doc = repo.create<TDoc>();
+    const doc = repo.create<TDoc>({ count: 0 });
+    expect(doc.docSync()?.count).to.eql(0);
     doc.change((d) => (d.count = 123));
     expect(doc.docSync()?.count).to.eql(123);
   });
